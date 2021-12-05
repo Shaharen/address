975f8f1a57331edbcaf68342f00da35564608bed
Choi_J_I <95062692+Shaharen@users.noreply.github.com>
2021-12-05 21:35:22 +0900

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,9 +1,9 @@
 package Exam02;
 
 public class Address {
-	private String name;
-	private int age;
-	private String phoneNumber;
+	private String name;	// 이름
+	private int age; 	// 나이
+	private String phoneNumber;//번호
 
 	public Address(String name, int age, String phoneNumber) {
 		this.name = name;
